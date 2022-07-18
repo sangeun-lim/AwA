d1863fecce449cbf64fb1daebd0b0fba074d4277
kimsejin <rlatpwls15@naver.com>
2022-07-18 16:16:17 +0900

    inital commit

---
+++ AwA/AwA/src/main/java/com/ssafy/AwA/AwAApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.AwA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwAApplication.class, args);
+	}
+
+}
