b639c42f8615bbba6bec249668f63a0eda233501
세룽룽 <zmmmm111@gmail.com>
2022-07-20 15:04:58 +0900

    Front: feat #Preview Page 제작

--- pjt-front/src/pages/Preview.tsx
+++ pjt-front/src/pages/Preview.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Preview.css";
 
 function Preview(): JSX.Element {
   return (
     <div>
       <h1>preview</h1>
+      <button className="previewBtn">
+        <Link to="/auth/login">LogIn</Link>
+      </button>
+      <button className="previewBtn">
+        <Link to="/">Home</Link>
+      </button>
     </div>
   );
 }
