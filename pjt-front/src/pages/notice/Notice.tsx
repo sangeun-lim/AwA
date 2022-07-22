b3d754c8fead81cc5376440a6490bd66afa4f84f
Seonhyuk <gomduridkstjsgur@gmail.com>
2022-07-22 20:26:35 +0900

    Front: feat # 공지사항 CRUD 기능 추가

--- pjt-front/src/pages/notice/Notice.tsx
+++ pjt-front/src/pages/notice/Notice.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import NoticeListItem from "../../component/NoticeListItem";
+import { dbService } from "../../fbase";
+import { NoticeItem } from "./../../Interface";
 
 function Notice(): JSX.Element {
+  const navigate = useNavigate();
+  const [noticeList, setNoticeList] = useState<Array<NoticeItem>>([]);
+
+  const onClick = () => {
+    navigate("/notice/edit");
+  };
+
+  const callNotice = async () => {
+    const q = query(
+      collection(dbService, "notice"),
+      orderBy("createdAt", "desc")
+    );
+
+    const notices = (await getDocs(q)).docs;
+
+    const newNotices: Array<NoticeItem> = notices.map((notice) => {
+      const { title, content, createdAt } = notice.data();
+      const newNotice: NoticeItem = {
+        title,
+        content,
+        createdAt,
+        id: notice.id,
+      };
+      return newNotice;
+    });
+
+    setNoticeList(newNotices);
+  };
+
+  useEffect(() => {
+    callNotice();
+  });
+
   return (
-    <div>
+    <>
       <h1>Notice</h1>
-    </div>
+      <button onClick={onClick}>글쓰기</button>
+      {noticeList.map((notice) => {
+        return (
+          <div key={notice.id}>
+            <NoticeListItem notice={notice}></NoticeListItem>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
--- pjt-front/src/pages/notice/NoticeDetail.tsx
+++ pjt-front/src/pages/notice/NoticeDetail.tsx
@@ -1,9 +1,124 @@
-import React from "react";
+import { deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { dbService } from "../../fbase";
+
+interface Editing {
+  title: string;
+  content: string;
+}
 
 function NoticeDetail(): JSX.Element {
+  const params = useParams();
+  const address = params.id;
+  const navigate = useNavigate();
+  const [notice, setNotice] = useState<any>(null);
+  const [onEdit, setOnEdit] = useState<boolean>(false);
+  const [editNotice, setEditNotice] = useState<Editing>({
+    title: notice.title,
+    content: notice.content,
+  });
+
+  const onChange = (e: any) => {
+    const { name, value } = e.target;
+
+    setEditNotice((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const onListClick = () => {
+    navigate("/notice");
+  };
+
+  const onEditClick = () => {
+    setOnEdit(!onEdit);
+  };
+
+  const onSubmit = async () => {
+    await updateDoc(doc(dbService, `notice/${address}`), {
+      title: editNotice.title,
+      content: editNotice.content,
+    });
+    setOnEdit(!onEdit);
+  };
+
+  const onDeleteClick = async () => {
+    const del: boolean = window.confirm("삭제하시겠습니까?");
+    if (del) {
+      await deleteDoc(doc(dbService, `notice/${address}`));
+      navigate("/notice");
+    }
+  };
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await getDoc(doc(dbService, `notice/${address}`));
+      const newData = data.data();
+
+      return newData;
+    };
+
+    if (address) {
+      const newData = loadData();
+      setNotice({
+        ...newData,
+        id: address,
+      });
+    } else {
+      navigate("/notice");
+    }
+  }, [address, navigate]);
+
+  useEffect(() => {
+    setEditNotice({
+      title: notice.title,
+      content: notice.content,
+    });
+  }, [onEdit, notice]);
+
   return (
     <div>
-      <h1>NoticeDetail</h1>
+      {onEdit ? (
+        <div>
+          <h1>NoticeEdit</h1>
+          <form onSubmit={onSubmit}>
+            <input
+              name="title"
+              type="text"
+              placeholder="제목"
+              value={editNotice.title || ""}
+              onChange={onChange}
+              required
+            />
+            <br></br>
+            <textarea
+              name="content"
+              value={editNotice.content || ""}
+              cols={30}
+              rows={10}
+              placeholder="내용을 입력해주세요"
+              onChange={onChange}
+              required
+            ></textarea>
+            <br></br>
+            <input type="submit" value="수정" />
+          </form>
+          <button onClick={onEditClick}>취소</button>
+        </div>
+      ) : (
+        <div>
+          <h1>NoticeDetail</h1>
+          <h2>{notice?.title}</h2>
+          <p>{notice?.content}</p>
+          <button onClick={onEditClick}>수정</button>
+          <button onClick={onDeleteClick}>삭제</button>
+          <button onClick={onListClick}>목록</button>
+        </div>
+      )}
     </div>
   );
 }
