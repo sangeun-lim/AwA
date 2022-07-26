5d47a7d2e7140c666130c2b8da45c8edf81b89cb
최영현 <chlasnmzx2@naver.com>
2022-07-27 01:52:56 +0900

    sns login test

---
+++ socialAuth-master/src/main/java/com/ssafy/AwA/SocialAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.AwA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialAuthApplication.class, args);
+    }
+
+}
