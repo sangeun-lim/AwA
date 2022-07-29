13871c0b18edc54b3e7de838e2cedebc2f09278d
최영현 <chlasnmzx2@naver.com>
2022-07-29 10:39:04 +0900

    test

--- socialAuth-master/src/main/java/com/ssafy/AwA/SocialAuthApplication.java
+++ socialAuth-master/src/main/java/com/ssafy/AwA/SocialAuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocialAuthApplication {
-
+//checkcheck
     public static void main(String[] args) {
         SpringApplication.run(SocialAuthApplication.class, args);
     }
