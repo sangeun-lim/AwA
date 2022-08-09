6e47f1524145e258b88efa84c11e4c2c17005fd5
Seonhyuk <gomduridkstjsgur@gmail.com>
2022-08-10 04:46:10 +0900

    Front: feat #옥션 무한스크롤 구현

--- pjt-front/src/pages/auction/Auction.tsx
+++ pjt-front/src/pages/auction/Auction.tsx
@@ -9,16 +9,20 @@ import { loadingActions } from "../../store";
 import { ArtworkItem, User } from "./../../Interface";
 import SearchComponent from "../../component/SearchComponent";
 import { Masonry } from "@mui/lab";
+import { useInView } from "react-intersection-observer";
 
 function Auction(): JSX.Element {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [ref, inView] = useInView();
 
   const [itemList, setItemList] = useState<ArtworkItem[]>([]);
   const userObject = useSelector(
     (state: { userObject: User | null }) => state.userObject
   );
+  const loading = useSelector((state: { loading: boolean }) => state.loading);
   const [pageNum, setPageNum] = useState<number>(1);
+  const [lastPage, setLastPage] = useState<number>(100);
 
   const onClick = () => {
     navigate("/auction/create");
@@ -30,6 +34,7 @@ function Auction(): JSX.Element {
       const response = await api.artwork.getArtworks(pageNum);
       setPageNum(pageNum + 1);
       setItemList((prev) => prev.concat(response.data.artworkResponseDto));
+      setLastPage(Math.floor(response.data.totalCount / 20) + 1);
       dispatch(loadingActions.toggle());
     } catch (err) {
       console.log(err);
@@ -37,34 +42,47 @@ function Auction(): JSX.Element {
     }
   };
 
-  useEffect(() => {}, []);
-
   useEffect(() => {
     /* eslint-disable */
     callAuction();
   }, []);
 
+  useEffect(() => {
+    if (inView && !loading && pageNum <= lastPage) {
+      callAuction();
+      setPageNum((prev) => prev + 1);
+    }
+  }, [inView, loading]);
+
   return (
-    <div id="auctionPage" className={style.auction}>
-      <section className={style.auctionTop}>
-        <SearchComponent />
-      </section>
-      <div>
-        {userObject && (
-          <button onClick={onClick} className={style.auctionButton}>
-            상품등록
-          </button>
-        )}
+    <div>
+      <div className={style.auction}>
+        <section className={style.auctionTop}>
+          <SearchComponent />
+        </section>
+        <div>
+          {userObject && (
+            <button onClick={onClick} className={style.auctionButton}>
+              상품등록
+            </button>
+          )}
+        </div>
+        <Masonry columns={{ sm: 2, md: 3, xl: 4 }} spacing={1}>
+          {itemList.map((item, i) => {
+            return (
+              <div key={i} className="grid-item">
+                <AuctionCard item={item}></AuctionCard>
+              </div>
+            );
+          })}
+        </Masonry>
       </div>
-      <Masonry columns={{ sm: 2, md: 3, lg: 4 }} spacing={1}>
-        {itemList.map((item, i) => {
-          return (
-            <div key={i} className="grid-item">
-              <AuctionCard item={item}></AuctionCard>
-            </div>
-          );
-        })}
-      </Masonry>
+      {pageNum > lastPage && (
+        <div className={style.lastBox}>
+          <span>마지막 게시글 입니다</span>
+        </div>
+      )}
+      <div ref={ref}>.</div>
     </div>
   );
 }
